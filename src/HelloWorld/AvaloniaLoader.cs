09bce8d45a091855afd02b8effe8b71f39e69241
SubParSuperCar <elliot_grimm_2008@yahoo.com>
2026-04-07 00:21:07 -0700

    Refactor solution

---
+++ src/HelloWorld/AvaloniaLoader.cs
@@ -0,0 +1,16 @@
+using Avalonia;
+using Godot;
+using JLeb.Estragonia;
+
+namespace HelloWorld;
+
+public partial class AvaloniaLoader : Node
+{
+	public override void _Ready()
+	{
+		AppBuilder
+			.Configure<App>()
+			.UseGodot()
+			.SetupWithoutStarting();
+	}
+}
